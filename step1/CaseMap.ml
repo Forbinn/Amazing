9a81f3dede33162af203bdb0b6b981714823f915
Jonathan TRAN <jonathan.tran@epitech.eu>
2013-05-01 18:43:38 +0000

    Deux algos mais qui fonctionnent pas

--- step1/CaseMap.ml
+++ step1/CaseMap.ml
@@ -24,7 +24,7 @@ let rec get_random_side id width height =
 let get_ref_case_from_id cases id =
   ref (List.nth cases id)
 
-let generate width height =
+(*let generate width height =
   let rec create_cases cases n =
     if n < 0 then cases
     else create_cases ((Case.create n n)::cases) (n - 1)
@@ -67,7 +67,49 @@ let generate width height =
               color_cases cases (Case.get_color !a) (Case.get_door !b) [];
               end;
             open_doors cases
-  in open_doors (create_cases [] (width * height - 1))
+  in open_doors (create_cases [] (width * height - 1))*)
+
+let generate width height =
+  let rec create_cases cases n =
+    if n = 0 then cases
+    else create_cases ((Case.create n n)::cases) (n - 1)
+  and create_list l size =
+    if size = 0 then l
+    else create_list ((size - 1)::l) (size - 1)
+  and randomize_list nl l size =
+    let rec remove l n = match l with
+      | (i::r) -> if n = 0 then r else i::(remove r (n - 1))
+      | [] -> []
+    in match l with
+      | (i::r) -> let x = (Random.int size)
+                  in randomize_list ((List.nth l x)::nl) (remove l x) (size - 1)
+      | [] -> nl
+  and color_cases cases color doors path = match doors with
+    | (id::r) -> let case = !(get_ref_case_from_id cases id)
+      in Case.change_color case color;
+      if not (List.exists ((=) id) path) then
+        color_cases cases color (Case.get_door case) (id::path)
+    | [] -> ()
+  and open_doors cases l = match l with
+    | (i::r) -> let a_id = i
+                in let b_id = (get_random_side a_id width height)
+                in let a = !(get_ref_case_from_id cases a_id)
+                   and b = !(get_ref_case_from_id cases b_id)
+                  in if (Case.get_color a) <> (Case.get_color b) then
+                    begin
+                      Case.add_door a b_id;
+                      Case.add_door b a_id;
+                      color_cases cases (Case.get_color a) (Case.get_door b) [];
+                    end;
+                    open_doors cases r
+    | [] -> ()
+  in let l = randomize_list []
+             (create_list [] (width * height)) (width * height)
+     and c = create_cases [] (width * height)
+    in begin
+      open_doors c l;
+      c;
+    end
 
 let create width height =
   {
