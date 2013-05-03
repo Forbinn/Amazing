a92787d26d247fed26abcf934168fc4aa6111682
Vins54 <vincent.leroy@epitech.eu>
2013-05-03 15:56:09 +0200

    Solver OK!!

--- step3/Solver.ml
+++ step3/Solver.ml
@@ -3,16 +3,20 @@ open CaseMap
 
 let solve map a_id b_id =
     let cases = CaseMap.get_case_list map in
-    let (case_a, case_b) = (List.nth cases a_id, List.nth cases b_id) in
+    let (case_a, _) = (List.nth cases a_id, List.nth cases b_id) in
 
     let rec find_path case path =
         let rec parcour_door doors path = match doors with
             | [] -> []
-            | door::r -> let found = find_path (List.nth cases door) door::path in
-                         if found = [] then
-                            parcour_door r path
-                         else
-                            found
+            | door::r -> if (List.exists ((=) door) path) then parcour_door r path
+                        else
+                          begin
+                            let found = find_path (List.nth cases door) (door::path) in
+                            if found = [] then
+                              parcour_door r path
+                            else
+                              found
+                          end
         in
     if (Case.get_id case) = b_id then
         path
